6fc45a259d8a8ed7df4124a040d29c1141ca8071
ChangyongKim0 <changyong.kim.0@gmail.com>
2023-06-24 02:27:35 +0000

    console deleted

--- src/component/RhinoModel.js
+++ src/component/RhinoModel.js
@@ -37,7 +37,7 @@ const RhinoModel = forwardRef(
     //   }
     // }, [modelgr]);
 
-    console.log(modelgr);
+    // console.log(modelgr);
 
     useImperativeHandle(ref, () => ({ model, modelgr }), [url]);
 
