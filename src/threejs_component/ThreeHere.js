f042e3d33fa0554cc767368eeb3f1ba941d91c37
ChangyongKim0 <changyong.kim.0@gmail.com>
2023-07-12 04:47:17 +0000

    make here modeling

---
+++ src/threejs_component/ThreeHere.js
@@ -0,0 +1,65 @@
+import React, {
+  Suspense,
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useLayoutEffect,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
+
+import * as THREE from "three";
+
+import { Canvas, useFrame, useGraph, useLoader } from "@react-three/fiber";
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  SoftShadows,
+  Sphere,
+  Stats,
+  useTexture,
+} from "@react-three/drei";
+import {
+  Selection,
+  Select,
+  EffectComposer,
+  Outline,
+} from "@react-three/postprocessing";
+import { BlendFunction, KernelSize } from "postprocessing";
+import { MathUtils } from "three";
+import useGlobalData from "../hooks/useGlobalData";
+import { useRhinoModel } from "../hooks/useRhinoModel";
+import { getS3URL } from "../hooks/useS3";
+
+const ThreeHere = forwardRef(({ position = [0, 0, 0], show = false }, ref) => {
+  const { children } = useRhinoModel(getS3URL("", "map/here_model.3dm"));
+  useEffect(() => {
+    console.log(position, show);
+  }, [position, show]);
+
+  const here = useRef();
+  useImperativeHandle(ref, () => here, [here]);
+
+  useFrame(() => {
+    if (show) {
+      here.current.rotateZ(-0.02);
+    }
+  });
+
+  return (
+    <group visible={show} position={position} ref={here}>
+      {children.map((e) => (
+        <mesh
+          key={e?.userData?.attributes?.id}
+          args={[e.geometry]}
+          userData={e?.userData}
+        >
+          <meshPhongMaterial attach={"material"} color={0x70a979} />
+        </mesh>
+      ))}
+    </group>
+  );
+});
+
+export default ThreeHere;
